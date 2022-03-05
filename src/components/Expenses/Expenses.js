38a5631581dad85df8e33e2d10d5f63e5ea714c6
Azie Melza Pratama <aziemp66@gmail.com>
2022-03-05 21:23:48 +0700

    Starting Project

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,21 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import "./Expenses.css";
+
+function Expenses(props) {
+    return (
+        <Card className="expenses">
+            {props.data.map((expense) => {
+                return (
+                    <ExpenseItem
+                        date={expense.date}
+                        title={expense.title}
+                        amount={expense.amount}
+                    />
+                );
+            })}
+        </Card>
+    );
+}
+
+export default Expenses;
