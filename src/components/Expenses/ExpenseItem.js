c2d6dbc12fd6a7c05025687d98f0658bc07fa7a3
Azie Melza Pratama <aziemp66@gmail.com>
2022-03-07 21:59:11 +0700

    Converting Stateful Component to Stateless

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,11 +1,8 @@
-import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title);
-
     const clickHandler = () => {
         setTitle(
             title === props.title ? props.title + " (edited)" : props.title
@@ -16,7 +13,7 @@ const ExpenseItem = (props) => {
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">$ {props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
